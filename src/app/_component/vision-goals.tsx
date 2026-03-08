c17aa8c4d9fd8c263f008359a773045a98a8dbdf
brunorwanda4 <brunorwanda4@gmail.com>
2026-03-08 08:50:25 +0200

    add vision goals and images

---
+++ src/app/_component/vision-goals.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+
+const goals = [
+  {
+    title: "Support Farmers",
+    description:
+      "Provide accessible digital tools that help farmers understand and manage crop health.",
+    image: "/jpg/farmers.jpg",
+  },
+  {
+    title: "Improve Food Security",
+    description:
+      "Reduce crop loss and increase agricultural productivity across Rwanda and Africa.",
+    image: "/jpg/food.jpg",
+  },
+  {
+    title: "Use Technology for Agriculture",
+    description:
+      "Leverage data, analysis, and intelligent systems to guide better farming practices.",
+    image: "/jpg/technology.jpg",
+  },
+];
+const VisionGoals = () => {
+  const [active, setActive] = useState(0);
+
+  const handleGoalClick = (index: number) => {
+    setActive(index);
+  };
+  return (
+    <main className=" flex gap-4">
+      <div className=" space-y-8 w-1/3">
+        {goals.map((goal, index) => (
+          <div
+            key={`${index}-${goal.title}`}
+            className={cn(
+              " cursor-pointer",
+              active === index &&
+                "border-secondary p-4 py-6 shadow-xl rounded-md border-l-6 ",
+            )}
+            onClick={() => handleGoalClick(index)}
+          >
+            <h3 className=" h3">{goal.title}</h3>
+            <p className="p">{goal.description}</p>
+          </div>
+        ))}
+      </div>
+      {/*images*/}
+      <div className=" w-2/3">
+        <div className="bg-base-200 h-full rounded-xl p-8 pr-0 pb-0">
+          <div className=" w-full h-full relative">
+            <Image
+              src={goals[active].image}
+              alt={goals[active].title}
+              fill
+              className=" rounded-tl-md rounded-br-md  object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default VisionGoals;
--- src/app/_component/vision.tsx
+++ src/app/_component/vision.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import VisionGoals from "./vision-goals";
 
 export default function Vision() {
   return (
-    <div id="vision" className=" min-h-screen pt-20">
+    <section id="vision" className=" min-h-screen pt-20 space-y-12">
       <div className=" flex flex-col gap-2">
-        <h2 className="text-3xl font-medium">Our Vision</h2>
+        <h2 className="h1">Our Vision</h2>
         <p className=" max-w-xl">
           To empower farmers in Rwanda and across Africa with intelligent tools
           that help them grow healthier crops, protect their harvests, and
           strengthen food security through accessible technology.
         </p>
       </div>
-    </div>
+      <VisionGoals />
+    </section>
   );
 }
